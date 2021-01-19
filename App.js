271683141be7fd869a5672d638e4087790c45c10
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-01-19 19:10:39 +0200

    Finished example.

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect, useState} from 'react';
+import { ScrollView, StyleSheet, Text, View, Image} from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [persons,setPersons] = useState([]);
+  
+  useEffect(() => {
+    const testArray = Array();
+    for (let i=0;i<=30;i++) {
+      testArray.push({id: i,name: 'Test ' + i, image: 'https://reactnative.dev/img/tiny_logo.png'});
+    }
+    setPersons(testArray);
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        {
+          persons.map((item) => (
+            <View style={styles.rowContainer} key={item.id}>
+              <Image
+                source={{
+                uri: item.image,
+                width: 32,
+                height: 32,
+                }}
+              />
+              <Text style={styles.rowText}>{item.name}</Text>
+            </View>
+          ))
+        }
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop:  Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft: 5,
+  },
+  rowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  rowText: {
+    fontSize: 20,
+    marginLeft: 5,
   },
 });
